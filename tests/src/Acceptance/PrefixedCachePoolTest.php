ac98f59463d2b8a3f67cd79dad97eadeb7e49b7b
Andor <david-andor@kozpontiagy.hu>
2023-05-07 21:56:11 +0200

    WIP 3.x Split

--- tests/src/Acceptance/PrefixedCachePoolTest.php
+++ tests/src/Acceptance/PrefixedCachePoolTest.php
@@ -12,7 +12,7 @@ declare(strict_types = 1);
  * with this source code in the file LICENSE.
  */
 
-namespace Cache\Prefixed\Tests;
+namespace Cache\Prefixed\Tests\Acceptance;
 
 use Cache\Prefixed\PrefixedCachePool;
 use PHPUnit\Framework\TestCase;
